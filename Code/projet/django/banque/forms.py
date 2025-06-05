65134c83af523915278111edc82f14b24b9056b6
Barrabbas21 <barrabbas21@gmail.com>
2025-06-05 09:51:57 +0200

    ajout forms.py

---
+++ Code/projet/django/banque/forms.py
@@ -0,0 +1,60 @@
+from django.forms import ModelForm
+from django.utils.translation import gettext_lazy as _
+from . import models
+
+class Categorie_FilmForm(ModelForm):
+    class Meta:
+        model = models.Categorie_Film
+        fields = ('nom', 'descriptif')
+        labels = {
+            'nom' : _('nom') ,
+            'descriptif' : _('descriptif'),
+        }
+
+
+class FilmForm(ModelForm):
+    class Meta:
+        model = models.Film
+        fields = ('titre', 'annee_sortie', 'affiche','realisateur','categorie')
+        labels = {
+            'titre' : _('titre') ,
+            'annee_sortie' : _('annee_sortie'),
+            'affiche' : _('affiche'),
+            'realisateur' : _('realisateur'),
+            'categorie' : _('categorie'),
+        }
+
+class ActeurForm(ModelForm):
+    class Meta:
+        model = models.Acteur
+        fields = ('nom', 'prenom', 'age','photos')
+        labels = {
+            'nom' : _('nom') ,
+            'prenom' : _('prenom'),
+            'age' : _('age'),
+            'photos' : _('photos'),
+        }
+
+class PersonnesForm(ModelForm):
+    class Meta:
+        model = models.Personnes
+        fields = ('pseudo', 'nom_prenom', 'mail','mot_de_passe','type')
+        labels = {
+            'pseudo' : _('pseudo') ,
+            'nom_prenom' : _('nom_prenom'),
+            'mail' : _('mail'),
+            'mot_de_passe' : _('mot_de_passe'),
+            'type' : _('type'),
+        }
+
+class CommentairesForm(ModelForm):
+    class Meta:
+        model = models.Commentaires
+        fields = ('film', 'personnes', 'note','commentaire','date')
+        labels = {
+            'film' : _('film') ,
+            'personnes' : _('personnes'),
+            'note' : _('note'),
+            'commentaire' : _('commentaire'),
+            'date' : _('date'),
+        }
