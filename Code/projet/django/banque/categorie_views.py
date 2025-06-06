098ee0ac360b3761a44810ecc0d5ed569efcfd07
Ignorantum <125766863+1gn0r4ntum@users.noreply.github.com>
2025-06-06 16:19:17 +0200

    views

--- Code/projet/django/banque/categorie_views.py
+++ Code/projet/django/banque/categorie_views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from .forms import FilmForm
+from .forms import Categorie_FilmForm
 from . import models
 # Create your views here.
 
@@ -17,3 +17,39 @@ def ajout(request):
         form = Categorie_FilmForm() 
         return render(request,"banque/categorie/ajout_categorie.html",{"form" : form})
         
+def traitement(request, id):
+    lform = Categorie_FilmForm(request.POST)
+    if lform.is_valid():
+        categorie = lform.save()
+        list_data = list(models.Categorie_Film.objects.all())
+        return render(request, "banque/categorie/all_categorie.html", {"liste" : list_data})
+    else:
+        return render(request,"banque/categorie/ajout_categorie.html",{"form": lform})
+
+def affiche_all(request):
+    list_data = list(models.Categorie_Film.objects.all())
+    return render(request, "banque/categorie/all_categorie.html", {"liste" : list_data})
+
+def modifier(request, id):
+    categorie = models.Categorie_Film.objects.get(id=id)
+    lform = Categorie_FilmForm(categorie.__dict__)
+    return render(request, "banque/categorie/modifier_categorie.html", {"lform" : lform, "id" : id})
+
+def sauvegarder_modif(request, id):
+    n_form = Categorie_FilmForm(request.POST)
+    if n_form.is_valid():
+        sauvegarde = n_form.save(commit=False)
+        sauvegarde.id = id
+        sauvegarde.save()
+        list_data = list(models.Magasin.objects.all())
+        return render(request, "banque/categorie/all_categorie.html", {"liste" : list_data})
+
+def supprimer(request, id):
+    categorie = models.Categorie_Film.objects.get(id=id)
+    lform = Categorie_FilmForm(categorie.__dict__)
+    return render(request, "banque/categorie/supprimer_categorie.html", {"lform" : lform, "id" : id})
+
+def stock(request, id):
+    categorie = models.Categorie_Film.objects.get(id=id)
+    list_data = list(models.Categorie_Film.objects.all())
+    return render(request, "banque/categorie/stock_categorie.html", {"liste" : list_data, "id" : id})
