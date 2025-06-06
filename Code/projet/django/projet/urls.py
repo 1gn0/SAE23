f7cc43dd66a4183ba26b95a798836340d3b8adc2
Barrabbas21 <barrabbas21@gmail.com>
2025-06-06 13:28:10 +0200

    Film

--- Code/projet/django/projet/urls.py
+++ Code/projet/django/projet/urls.py
@@ -1,43 +1,22 @@
+"""
+URL configuration for projet project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/5.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
 from django.urls import path
-from . import views
 
 urlpatterns = [
-
-    path('Acteur/modifier_acteur/<int:id>/', views.modifier_acteur),
-    path('Acteur/supprimer_acteur/<int:id>/', views.supprimer_acteur),
-    path('Acteur/ajout_acteur/', views.ajout_acteur),
-    path('Acteur/affiche_acteur/<int:id>/', views.affiche_acteur),
-    path('Acteur/traitement/', views.traitement_acteur),
-
-
-    path('Categorie_Film/modifier_categorie_film/<int:id>/', views.modifier_categorie_film),
-    path('Categorie_Film/supprimer_categorie_film/<int:id>/', views.supprimer_categorie_film),
-    path('Categorie_Film/ajout_categorie_film/', views.ajout_categorie_film),
-    path('Categorie_Film/affiche_categorie_film/<int:id>/', views.affiche_categorie_film),
-    path('Categorie_Film/traitement/', views.traitement_categorie_film),
-
-    path('Commentaire/modifier_commentaire/<int:id>/', views.modifier_commentaire),
-    path('Commentaire/supprimer_commentaire/<int:id>/', views.supprimer_commentaire),
-    path('Commentaire/ajout_commentaire/', views.ajout_commentaire),
-    path('Commentaire/affiche_commentaire/<int:id>/', views.affiche_commentaire),
-    path('Commentaire/traitement/', views.traitement_commentaire),
-
-
-    path('Film/modifier_film/<int:id>/', views.modifier_film),
-    path('Film/supprimer_film/<int:id>/', views.supprimer_film),
-    path('Film/ajout_film/', views.ajout_film),
-    path('Film/affiche_film/<int:id>/', views.affiche_film),
-    path('Film/traitement/', views.traitement_film),
-
-
-    path('Personne/modifier_personne/<int:id>/', views.modifier_personne),
-    path('Personne/supprimer_personne/<int:id>/', views.supprimer_personne),
-    path('Personne/ajout_personne/', views.ajout_personne),
-    path('Personne/affiche_personne/<int:id>/', views.affiche_personne),
-    path('Personne/traitement/', views.traitement_personne),
-
-
-    path('main/', views.main),
-
-
+    path('admin/', admin.site.urls),
 ]
