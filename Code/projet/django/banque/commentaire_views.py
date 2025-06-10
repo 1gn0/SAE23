887be7b1189ba2f04f1e0d464396a8b686ea7204
Barrabbas21 <barrabbas21@gmail.com>
2025-06-10 14:27:22 +0200

    comment_views

--- Code/projet/django/banque/commentaire_views.py
+++ Code/projet/django/banque/commentaire_views.py
@@ -18,3 +18,41 @@ def ajout(request):
     else :
         form = CommentaireForm() 
         return render(request,"banque/Commentaire/commentaire.html",{"form" : form})
+    
+def traitement(request, id):
+    lform = CommentaireForm(request.POST)
+    if lform.is_valid():
+        commentaire = lform.save()
+        list_data = list(models.Commentaire.objects.all())
+        return render(request, "banque/Commentaire/all_commentaire.html", {"liste" : list_data})
+    else:
+        return render(request,"banque/Commentaire/ajout_commentaire.html",{"form": lform})
+
+def affiche_all(request):
+    list_data = list(models.Commentaire.objects.all())
+    return render(request, "banque/Commentaire/all_commentaire.html", {"liste" : list_data})
+
+def modifier(request, id):
+    commentaire = models.Commentaire.objects.get(id=id)
+    lform = CommentaireForm(commentaire.__dict__)
+    return render(request, "banque/Commentaire/modifier_commentaire.html", {"lform" : lform, "id" : id})
+
+def sauvegarder_modif(request, id):
+    n_form = CommentaireForm(request.POST)
+    if n_form.is_valid():
+        sauvegarde = n_form.save(commit=False)
+        sauvegarde.id = id
+        sauvegarde.save()
+        list_data = list(models.Magasin.objects.all())
+        return render(request, "banque/Commentaire/all_commentaire.html", {"liste" : list_data})
+
+def supprimer(request, id):
+    commentaire = models.Commentaire.objects.get(id=id)
+    lform = CommentaireForm(commentaire.__dict__)
+    return render(request, "banque/Commentaire/supprimer_commentaire.html", {"lform" : lform, "id" : id})
+
+def stock(request, id):
+    commentaire = models.Commentaire.objects.get(id=id)
+    list_data = list(models.Commentaire.objects.all())
+    return render(request, "banque/Commentaire/stock_commentaire.html", {"liste" : list_data, "id" : id})
+
