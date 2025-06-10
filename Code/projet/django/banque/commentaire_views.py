023c0d930f0a2cbdbb4fb840ffc681d057844d5c
Barrabbas21 <barrabbas21@gmail.com>
2025-06-10 14:20:49 +0200

    views

--- Code/projet/django/banque/commentaire_views.py
+++ Code/projet/django/banque/commentaire_views.py
@@ -1,13 +1,5 @@
 from django.shortcuts import render
 from .forms import CommentaireForm
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-from . import models
-
-from django.shortcuts import render
-from .forms import CommentaireForm
->>>>>>> 8d26062502397acc8161227b8fb45723d01d325b
 from . import models
 
 from .models import Commentaire
@@ -22,72 +14,7 @@ def ajout(request):
             list_data = list(models.Commentaire.objects.all())
             return render(request, "banque/Commentaire/affiche_commentaire.html", {"liste" : list_data})
         else:
-<<<<<<< HEAD
             return render(request,"banque/Commentaire/ajout_commentaire.html",{"form": form})
     else :
         form = CommentaireForm() 
         return render(request,"banque/Commentaire/commentaire.html",{"form" : form})
-=======
-from . import models
->>>>>>> 098ee0ac360b3761a44810ecc0d5ed569efcfd07
-=======
-            return render(requegst,"banque/Commentaire/ajout_commentaire.html",{"form": form})
-    else :
-        form = CommentaireForm() 
-        return render(request,"banque/Commentaire/commentaire.html",{"form" : form})
-
-def traitement(request, id):
-    film = models.Film.objects.get(id=id)
-    form = GuitareForm(request.POST)
-    if form.is_valid():
-        commentaire = form.save(commit=False)
-        commentaire.film = film
-        commentaire.save()
-        return redirect('stock', id=commentaire.id)
-    else:
-        return render(request, "banque/commentaire/ajout.html", {"form": form, "id": id})
-
-<<<<<<< HEAD
-def affiche_all(request):
-    list_data = list(models.Film.objects.all())
-    return render(request, "banque/commentaire/affiche_commentaire.html", {"liste" : list_data})
-    
-=======
-
-def read(request, id):
-    film = models.Film.objects.get(id=id)
-    return render(request, "banque/commentaire/affiche_commentaire.html", {"film" : film})
-<<<<<<< HEAD
->>>>>>> 8d26062502397acc8161227b8fb45723d01d325b
-=======
-
->>>>>>> 0f0d3f83b97aeaa3724a3806c50edae39afc18ae
-def modifier(request, id):
-    commentaire = models.Commentaire.objects.get(id=id)
-    lform = CommentaireForm(commentaire.__dict__)
-    return render(request, "banque/commentaire/modifier_commentaire.html", {"lform" : lform, "id" : id})
-
-def sauvegarder_modif(request, id):
-    n_form = CommentaireForm(request.POST)
-    if n_form.is_valid():
-        sauvegarde = n_form.save(commit=False)
-        sauvegarde.id = id
-        sauvegarde.save()
-        list_data = list(models.Film.objects.all())
-        return render(request, "banque/commentaire/all_commentaire.html", {"liste" : list_data})
-
-def supprimer(request, id):
-    commentaire = models.Commentaire.objects.get(id=id)
-    if n_form.is_valid():
-        sauvegarde = n_form.save(commit=False)
-        sauvegarde.id = id
-        sauvegarde.save()
-        list_data = list(models.Film.objects.all())
-        return render(request, "banque/commentaire/all_commentaire.html", {"liste" : list_data})
-
-def suppression(request, id):
-    commentaire = models.Commentaire.objects.get(id=id)
-    commentaire.delete()
-    list_data = list(models.Film.objects.all())
-    return render(request, "banque/commentaire/all_commentaire.html", {"liste" : list_data})
->>>>>>> 2e34b2ca496337786251209473e6095307066b5a
--- Code/projet/django/banque/film_views.py
+++ Code/projet/django/banque/film_views.py
@@ -10,11 +10,45 @@ def ajout(request):
     if request.method == "POST": 
         form = FilmForm(request)
         if form.is_valid():
-            Film= form.save() 
+            film= form.save() 
             list_data = list(models.Film.objects.all())
-            return render(request, "banque/Film/affiche_film.html", {"liste" : list_data})
-        else:
-            return render(request,"banque/Film/ajout_film.html",{"form": form})
-    else :
-        form = FilmForm() 
-        return render(request,"banque/Film/film.html",{"form" : form})
+            return render(request,"banque/Film/ajout_film.html",{"form" : form})
+        else :       
+            return render(request,"banque/Film/ajout_film.html",{"form" : form})
+        
+def traitement(request, id):
+    lform = FilmForm(request.POST)
+    if lform.is_valid():
+        film = lform.save()
+        list_data = list(models.Film.objects.all())
+        return render(request, "banque/Film/all_film.html", {"liste" : list_data})
+    else:
+        return render(request,"banque/Film/ajout_film.html",{"form": lform})
+
+def affiche_all(request):
+    list_data = list(models.Film.objects.all())
+    return render(request, "banque/Film/all_film.html", {"liste" : list_data})
+
+def modifier(request, id):
+    categorie = models.Film.objects.get(id=id)
+    lform = FilmForm(categorie.__dict__)
+    return render(request, "banque/Film/modifier_film.html", {"lform" : lform, "id" : id})
+
+def sauvegarder_modif(request, id):
+    n_form = FilmForm(request.POST)
+    if n_form.is_valid():
+        sauvegarde = n_form.save(commit=False)
+        sauvegarde.id = id
+        sauvegarde.save()
+        list_data = list(models.Magasin.objects.all())
+        return render(request, "banque/Film/all_film.html", {"liste" : list_data})
+
+def supprimer(request, id):
+    film = models.Film.objects.get(id=id)
+    lform = FilmForm(film.__dict__)
+    return render(request, "banque/Film/supprimer_film.html", {"lform" : lform, "id" : id})
+
+def stock(request, id):
+    film = models.Film.objects.get(id=id)
+    list_data = list(models.Film.objects.all())
+    return render(request, "banque/Film/stock_film.html", {"liste" : list_data, "id" : id})
